a5135ba052f158a01e33fd86be6dab5a9fff5150
Ricardo Marquez <lasuerteloestoda@gmail.com>
2018-11-20 08:32:32 -0500

    pregunta 02 fin

--- Pregunta02.py
+++ Pregunta02.py
@@ -5,10 +5,23 @@ c=int(input("Ingrese el tercer numero"))
 d=int(input("Ingrese el cuarto numero"))
 e=int(input("Ingrese el quinto numero"))
 
-def "menor_en_arreglo"(self, parameter_list):
-    
-    arreglo[]=[a, b, c, d, e]
-    if a<b and a<c and a<d and a<e:
-    arreglo()= [b, c, d, e]
-    
+# Proceso
+
+if a<b and a<c and a<d and a<e:
+    arreglo= [b, c, d, e]
+
+elif b<a and b<c and b<d and b<e:
+    arreglo= [a, c, d, e]
+
+elif c<a and c<b and c<d and c<e:
+    arreglo= [a, b, d, e]
+
+elif d<a and d<b and d<c and d<e:
+    arreglo= [a, b, c, e]
+
+elif e<a and e<b and e<c and e<d:
+    arreglo= [a, b, c, d]
+
+# Salida
+
 print(arreglo)
