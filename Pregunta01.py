e177aa56a6bd3821ee9e5e5e578963cc66e7599a
Angello Cruz <angellocruz0911@gmail.com>
2018-11-20 08:00:07 -0500

    pregunta 1 finalizada

---
+++ Pregunta01.py
@@ -0,0 +1,16 @@
+import math
+# Entrada
+x=int(input("Ingrese la coordenada x"))
+y=int(input("Ingrese la coordenada y"))
+
+# Proceso
+raiz=math.sqrt
+
+distancia= raiz((x*x)+(y*y))
+
+# Salida
+print("La distancia entre el punto indicado y el punto 0,0 es:")
+print(distancia)
+
+
+
---
+++ Pregunta02.py
@@ -0,0 +1,14 @@
+# Entrada
+a=int(input("Ingrese el primer numero"))
+b=int(input("Ingrese el segundo numero"))
+c=int(input("Ingrese el tercer numero"))
+d=int(input("Ingrese el cuarto numero"))
+e=int(input("Ingrese el quinto numero"))
+
+def "menor_en_arreglo"(self, parameter_list):
+    
+    arreglo[]=[a, b, c, d, e]
+    if a<b and a<c and a<d and a<e:
+    arreglo()= [b, c, d, e]
+    
+print(arreglo)
